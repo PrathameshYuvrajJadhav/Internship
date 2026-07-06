654660616c1601279800fe5aadd4ac7bc1b707fb
PrathameshYuvrajJadhav <prathamjadhav1408@gmail.com>
2026-07-07 01:10:14 +0530

    just made a to-do list application

---
+++ test/Project Assignment/To-Do List Application using HTML, CSS, Bootstrap and JavaScript/script.js
@@ -0,0 +1,67 @@
+let tasks = [];
+
+// Load tasks from localStorage
+if (localStorage.getItem("tasks") != null) {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+// Show tasks when page loads
+showTasks();
+
+// Add Task Event
+document.getElementById("addTaskBtn").addEventListener("click", function () {
+
+    let task = document.getElementById("taskInput").value;
+
+    if (task == "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    document.getElementById("taskInput").value = "";
+
+    showTasks();
+});
+
+// Display Tasks
+function showTasks() {
+
+    let output = "";
+
+    tasks.forEach(function (task, index) {
+
+        output += `
+            <li class="list-group-item">
+                ${task}
+
+                <button class="delete-btn"
+                    onclick="deleteTask(${index})">
+                    Delete
+                </button>
+            </li>
+        `;
+    });
+
+    let taskList = document.getElementById("taskList");
+
+    if (tasks.length === 0) {
+        taskList.innerHTML = "<li class='list-group-item'>No Tasks Available</li>";
+    }
+    else {
+        taskList.innerHTML = output;
+    }
+}
+
+// Delete Task
+function deleteTask(index) {
+
+    tasks.splice(index, 1);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    showTasks();
+}
